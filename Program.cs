21e355b627ad4282a02ca7db22c22dea5d0538ec
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-31 03:01:33 +0530

    FrequencyOfWordsInSentence

--- Program.cs
+++ Program.cs
@@ -22,49 +22,49 @@ namespace HashTable
 
 
 
-            Console.WriteLine("Hash table demo");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] paraWords = para.Split(' ');
-            int pLength = paraWords.Length;
-            for (int i = 0; i < pLength; i++)
-            {
-                hash.Add(Convert.ToString(i), paraWords[i]);
-            }
-            foreach (string word in paraWords)
-            {
-                hash.GetFrequency(word);
-            }
-            Console.ReadKey();
+            //Console.WriteLine("Hash table demo");
+            //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            //string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            //string[] paraWords = para.Split(' ');
+            //int pLength = paraWords.Length;
+            //for (int i = 0; i < pLength; i++)
+            //{
+            //    hash.Add(Convert.ToString(i), paraWords[i]);
+            //}
+            //foreach (string word in paraWords)
+            //{
+            //    hash.GetFrequency(word);
+            //}
+            //Console.ReadKey();
 
 
 
 
-            Console.WriteLine("Hash table demo");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] paraWords = para.Split(' ');
-            int pLength = paraWords.Length;
-            for (int i = 0; i < pLength; i++)
-            {
-                hash.Add(Convert.ToString(i), paraWords[i]);
-            }
+            //Console.WriteLine("Hash table demo");
+            //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            //string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            //string[] paraWords = para.Split(' ');
+            //int pLength = paraWords.Length;
+            //for (int i = 0; i < pLength; i++)
+            //{
+            //    hash.Add(Convert.ToString(i), paraWords[i]);
+            //}
 
-            Console.WriteLine("Frequency of 'avoidable' before removal is :" + hash.GetFrequency("avoidable"));
-            hash.RemoveValue("avoidable");
-            Console.WriteLine("Frequency of 'avoidable' after removal is :" + hash.GetFrequency("avoidable"));
-            Console.ReadKey();
-
-
-            Console.WriteLine("Hello!! Welcome to Binary Search Tree");
-            BST<int> bst = new BST<int>();
+            //Console.WriteLine("Frequency of 'avoidable' before removal is :" + hash.GetFrequency("avoidable"));
+            //hash.RemoveValue("avoidable");
+            //Console.WriteLine("Frequency of 'avoidable' after removal is :" + hash.GetFrequency("avoidable"));
+            //Console.ReadKey();
 
 
-            bst.Insert(56);
-            bst.Insert(30);
-            bst.Insert(70);
+            //Console.WriteLine("Hello!! Welcome to Binary Search Tree");
+            //BST<int> bst = new BST<int>();
+
+
+            //bst.Insert(56);
+            //bst.Insert(30);
+            //bst.Insert(70);
             
-            bst.DisplayBST(bst.root);
+            //bst.DisplayBST(bst.root);
 
 
             //int[] arr = { 56, 30, 70, 22, 60, 40, 95, 16, 63, 67, 4, 11, 65 };
