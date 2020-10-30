f34a9f67288100a26a70eb27271a851a44ec6941
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-10-31 03:24:48 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hash table demo");
-            //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            //hash.Add("0", "To");
-            //hash.Add("1", "be");
-            //hash.Add("2", "or");
-            //hash.Add("3", "not");
-            //hash.Add("4", "to");
-            //hash.Add("5", "be");
-            //string checkFreq = "be";
-            //int freq = hash.GetFrequency(checkFreq);
-            //Console.ReadKey();
+            Console.WriteLine("Hash table demo");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string checkFreq = "be";
+            int freq = hash.GetFrequency(checkFreq);
+            Console.ReadKey();
 
 
 
