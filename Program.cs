24ef4cee0dfb9e5bfa1162768373cd2fa6875c7e
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-10-31 03:27:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hash table demo");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-            string checkFreq = "be";
-            int freq = hash.GetFrequency(checkFreq);
-            Console.ReadKey();
+            //Console.WriteLine("Hash table demo");
+            //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            //hash.Add("0", "To");
+            //hash.Add("1", "be");
+            //hash.Add("2", "or");
+            //hash.Add("3", "not");
+            //hash.Add("4", "to");
+            //hash.Add("5", "be");
+            //string checkFreq = "be";
+            //int freq = hash.GetFrequency(checkFreq);
+            //Console.ReadKey();
 
 
 
@@ -40,20 +40,20 @@ namespace HashTable
 
 
 
-            //Console.WriteLine("Hash table demo");
-            //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            //string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            //string[] paraWords = para.Split(' ');
-            //int pLength = paraWords.Length;
-            //for (int i = 0; i < pLength; i++)
-            //{
-            //    hash.Add(Convert.ToString(i), paraWords[i]);
-            //}
+            Console.WriteLine("Hash table demo");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] paraWords = para.Split(' ');
+            int pLength = paraWords.Length;
+            for (int i = 0; i < pLength; i++)
+            {
+                hash.Add(Convert.ToString(i), paraWords[i]);
+            }
 
-            //Console.WriteLine("Frequency of 'avoidable' before removal is :" + hash.GetFrequency("avoidable"));
-            //hash.RemoveValue("avoidable");
-            //Console.WriteLine("Frequency of 'avoidable' after removal is :" + hash.GetFrequency("avoidable"));
-            //Console.ReadKey();
+            Console.WriteLine("Frequency of 'avoidable' before removal is :" + hash.GetFrequency("avoidable"));
+            hash.RemoveValue("avoidable");
+            Console.WriteLine("Frequency of 'avoidable' after removal is :" + hash.GetFrequency("avoidable"));
+            Console.ReadKey();
 
 
             //Console.WriteLine("Hello!! Welcome to Binary Search Tree");
